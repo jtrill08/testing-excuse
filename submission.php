3ba4d79d433edb239b8b87011911e1e584ba87e7
jtrill08 <49694135+jtrill08@users.noreply.github.com>
2019-05-22 21:39:40 +0100

    Update submission.php

--- submission.php
+++ submission.php
@@ -9,8 +9,8 @@
 	$email_subject = "New Form Submission"; 
 
 	$email_body = "User Name: $name.\n". 
-					"User Email: $visitor_email.\n". 
-					"User Message: $message.\n";
+			"User Email: $visitor_email.\n". 
+			"User Message: $message.\n";
 
 
 	$to = "justiciapremo@outlook.com";
@@ -21,7 +21,7 @@
 
 	mail($to,$email_subject,$email_body,$headers); 
 
-	header("Location: page.html");
+	header("Location: index.html");
 
 
 
