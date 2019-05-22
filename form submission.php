c6eb8cd29834d94be20b3848afb363710457cfbc
jtrill08 <49694135+jtrill08@users.noreply.github.com>
2019-05-22 19:29:56 +0100

    first commit

---
+++ form submission.php
@@ -0,0 +1,16 @@
+<! DOCTYPE html> 
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <title>submit an excuse!</title>
+        <link rel="stylesheet" href="css/submission.css">
+    </head>
+    <body>
+        <main>
+            <P>submit an excuse!</P>
+            <form class="contact-form" action="contactform.php" method="post">
+                <textarea name="Excuse" placeholder="submit your excuse"></textarea>
+                <button type="submit" name="submit">submit</button>
+        </main>
+    </body>
+</html>
