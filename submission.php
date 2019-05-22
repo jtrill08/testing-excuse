6a0113c6f0e669bc107c3b16a7d4708800a3af4c
jtrill08 <49694135+jtrill08@users.noreply.github.com>
2019-05-22 21:31:41 +0100

    Update submission.php

--- submission.php
+++ submission.php
@@ -1,12 +1,28 @@
 <?php
-if(isset($_POST['submit'])) {
-	$name=$_POST['name'];
-	$subject=$_POST['subject'];
-	$mailFrom=$_POST['mail'];
-	$message=$_POST['message'];
-	$mailTo = "justiciapremo@outlook.com";
-	$headers= "From: ".$mailFrom; 
-	$txt = "You have recieved an e-email from ".$name.".\n\n".$message;
-	mail($mailTo, $subject, $txt, $headers);
-	header("Location: page.html mailsend");
-}
+	$name = $_POST ['name']; 
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message']; 
+
+
+	$email_form = 'EasyTutorials@avinashk.com'; 
+
+	$email_subject = "New Form Submission"; 
+
+	$email_body = "User Name: $name.\n". 
+					"User Email: $visitor_email.\n". 
+					"User Message: $message.\n";
+
+
+	$to = "justiciapremo@outlook.com";
+
+	$headers = "From: $email_from \r\n"; 
+
+	$headers .= "Reply-To: $visitor_email \r\n";
+
+	mail($to,$email_subject,$email_body,$headers); 
+
+	header("Location: page.html");
+
+
+
+
