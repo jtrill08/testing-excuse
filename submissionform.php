b7f811cf4d86db88950587c7b4debd43a8f6e349
jtrill08 <49694135+jtrill08@users.noreply.github.com>
2019-05-23 00:10:40 +0100

    Create submissionform.php

---
+++ submissionform.php
@@ -0,0 +1,15 @@
+ <?php
+
+if(isset($_POST['submit'])) {
+	$name=$_POST['name'];
+	$Excuse=$_POST['Excuse'];
+
+
+	$mailTo = "justiciapremo@outlook.com";
+	$txt = "You have recieved an e-email from ".$name.".\n\n".$Excuse;
+
+
+	mail($mailTo,$txt);
+	header("Location: submission1.php?mailsend");
+
+}
